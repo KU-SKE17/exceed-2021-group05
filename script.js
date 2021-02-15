dc104de358b668d60effc460d9a5c97c17a77000
Nanthakarn Limkool <nanthakarn18@gmail.com>
2021-02-16 01:37:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// match data
+// TODO: match data
 function update(info) {
     // set ui ...
     // info.timestamp*1000
     // ...
 }
 
+// TODO: add url
 function loadData() {
-    // Todo: add url
     var url = "";
     fetch(url, {
         method: "GET",
@@ -20,3 +20,33 @@ function loadData() {
             })
         );
 }
+
+// TODO: add params
+function createSetting(p1, p2, p3) {
+    var url = "";
+    fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ p1: p1, p2: p2 }),
+    }).then((response) => console.log(response));
+}
+
+// TODO: add params
+function updateSetting(p1, p2, p3) {
+    var url = "";
+    fetch(url, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ p1: p1, p2: p2  }),
+    }).then((response) => console.log(response));
+}
+
+// // TODO: 
+// form.addEventListener("submit", event => {
+//     event.preventDefault();
+//     p1 = document.getElementById("p1").value;
+//     p2 = document.getElementById("p2").value;
+//     updateSetting(p1, p2);
+//     form.elements["p1"].value = "";
+//     form.elements["p2"].value = "";
+// })
