cf2a8b967a9ae6161018d6e535a9389fe245c2ec
Nanthakarn Limkool <nanthakarn18@gmail.com>
2021-02-17 02:50:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-// TODO: match data
-function update(info) {
-    // set ui ...
-    // info.timestamp*1000
-    // ...
+// current room
+let current_room;
+// const current_room_name = document.getElementById("room_name");
+
+// current data
+let current_data;
+// const current_time = document.getElementById("current_time");
+// const current_quality = document.getElementById("current_quality");
+// const current_temperature = document.getElementById("current_temperature");
+// const current_LPG = document.getElementById("current_LPG");
+// const current_CO = document.getElementById("current_CO");
+// const current_CH4 = document.getElementById("current_CH4");
+// const current_H2 = document.getElementById("current_H2");
+// const current_humidity = document.getElementById("current_humidity");
+
+// setting
+const setting_room_name = document.getElementById("setting_name");
+const setting_owner = document.getElementById("setting_owner");
+const setting_LPG = document.getElementById("setting_LPG");
+const setting_CO = document.getElementById("setting_CO");
+const setting_CH4 = document.getElementById("setting_CH4");
+const setting_H2 = document.getElementById("setting_H2");
+const setting_humidity = document.getElementById("setting_humidity");
+const setting_climatize = document.getElementById("setting_climatize");
+
+function getRoom(info) {
+    current_room = {
+        "room_name": info.room_name,
+        "room_owner": info.room_owner,
+        "warning_level": info.warning_level,
+        "auto_climatize": info.auto_climatize,
+    };
 }
 
-// TODO: add url
-function loadData() {
+// TODO: add url, del param
+function loadRoom(room_name) {
     var url = "";
     fetch(url, {
         method: "GET",
@@ -15,38 +42,73 @@ function loadData() {
         .then((response) => response.json())
         .then((datas) =>
             datas.result.forEach((data) => {
-                update(data);
+                getRoom(data);
                 // console.log(data);
             })
         );
 }
 
-// TODO: add params
-function createSetting(p1, p2, p3) {
-    var url = "";
-    fetch(url, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ p1: p1, p2: p2 }),
-    }).then((response) => console.log(response));
+function getCurrentData(info) {
+    current_data = {
+        "current_time": info.timestamp * 1000,
+        "quality": info.quality,
+        "temperature": info.temperature,
+        "LPG": info.LPG,
+        "CO": info.CO,
+        "CH4": info.CH4,
+        "H2": info.H2,
+        "humidity": info.humidity
+    };
 }
 
-// TODO: add params
-function updateSetting(p1, p2, p3) {
+// TODO: add url, del param
+function loadCurrentData(room_name) {
+    var url = "";
+    fetch(url, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    })
+        .then((response) => response.json())
+        .then((datas) =>
+            datas.result.forEach((data) => {
+                getCurrentData(data);
+                // console.log(data);
+            })
+        );
+}
+
+// function updateUI() {
+//     current_time.innerHTML = current_data["current_time"];
+// }
+
+// TODO: add names, url
+function updateSetting(rname, owner, warning, humidity, climatize) {
+    var new_setting = {
+        '': rname,
+        '': owner,
+        '': warning,
+        '': humidity,
+        '': climatize
+    }
     var url = "";
     fetch(url, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ p1: p1, p2: p2  }),
+        body: JSON.stringify(new_setting),
     }).then((response) => console.log(response));
 }
 
-// // TODO: 
-// form.addEventListener("submit", event => {
-//     event.preventDefault();
-//     p1 = document.getElementById("p1").value;
-//     p2 = document.getElementById("p2").value;
-//     updateSetting(p1, p2);
-//     form.elements["p1"].value = "";
-//     form.elements["p2"].value = "";
-// })
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    var rname = setting_room_name.value;
+    var owner = setting_owner.value;
+    var warning = {
+        "LPG": setting_LPG.value,
+        "CO": setting_CO.value,
+        "CH4": setting_CH4.value,
+        "H2": setting_H2.value,
+    };
+    var humidity = setting_humidity.value;
+    var climatize = setting_climatize.value;
+    updateSetting(rname, owner, warning, humidity, climatize);
+})
