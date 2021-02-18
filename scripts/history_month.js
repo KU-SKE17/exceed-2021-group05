1f4ac1f04b75bb03ed238f36fff21e94d600c442
SuphanutChandee <62004966+SuphanutChandee@users.noreply.github.com>
2021-02-18 21:03:52 +0700

    Update history_month.js

--- scripts/history_month.js
+++ scripts/history_month.js
@@ -76,7 +76,10 @@ window.onload = function ()
     //console.log(datee);
     
     //set sec to month
-    var index_to_day = (datee-datte)*86400000;
+    //var index_to_day = (datee-datte)*86400000;
+    //demo
+    var index_to_day = 6;
+    //console.log(index_to_day);
     var ct = 0;
     function add_new(arr_temp, arr_index, yValue, dps_arr, check_time)
     {
