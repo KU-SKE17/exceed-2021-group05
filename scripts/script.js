9b97c1f69e0e95eeea090c81d665b965c4955ef7
Nanthakarn Limkool <nanthakarn18@gmail.com>
2021-02-19 23:25:33 +0700

    set time in menu bar

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const current_time = document.getElementById("time");
+
+function setTime() {
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var d = new Date();
+    var day = days[d.getDay()];
+    var hr = d.getHours();
+    var min = d.getMinutes();
+    if (min < 10) {
+        min = "0" + min;
+    }
+    var ampm = "am";
+    if (hr > 12) {
+        hr -= 12;
+        ampm = "pm";
+    }
+    var date = d.getDate();
+    var month = months[d.getMonth()];
+    var year = d.getFullYear();
+    current_time.innerHTML = day + " " + hr + ":" + min + " " + ampm + " " + date + " " + month + " " + year;
+}
+setInterval(() => {
+    setTime()
+}, 1000);
